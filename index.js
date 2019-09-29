15cebeb4c7fc031718157d1c794ed4d6d1d10dbe
Jonster5 <49014692+Jonster5@users.noreply.github.com>
2019-09-29 00:37:09 -0400

    ok node test

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ let express = require('express');
 
 let app = express();
 
-//app.use(express.static('public'));
-
 let server = app.listen(PORT);
 
 let io = require('socket.io')(server);
 
-io.on('connection', function (socket) {
-  socket.emit('news', {hello: 'world'});
+io.on('connection', function(socket) {
+    io.socket.emit('news', { hello: 'world' });
 });
