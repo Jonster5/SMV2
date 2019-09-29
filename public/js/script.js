dca4a1c5cd3c7a925ac874301d0db6ddd33d29a6
Jonster5 <49014692+Jonster5@users.noreply.github.com>
2019-09-29 00:42:57 -0400

    o

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,27 @@
 function expand(a) {
-  let article = document.getElementById(a);
-  let a_h = a + "_h";
-  article.style.width = "540px";
-  document.getElementById(a_h).setAttribute("style", "display: inline-block;");
+    let article = document.getElementById(a);
+    let a_h = a + "_h";
+    article.style.width = "540px";
+    document.getElementById(a_h).setAttribute("style", "display: inline-block;");
 }
 
 function reset(a) {
-  let article = document.getElementById(a);
-  let a_h = a + "_h";
-  article.style.width = "270px";
-  document.getElementById(a_h).setAttribute("style", "display: none;");
+    let article = document.getElementById(a);
+    let a_h = a + "_h";
+    article.style.width = "270px";
+    document.getElementById(a_h).setAttribute("style", "display: none;");
 }
 
 function buttons(wo) {
-  switch (wo) {
-    case 1: location.assign("/tutorials"); break;
-    case 2: location.assign("/codepebble"); break;
-    case 3: location.assign("/games"); break;
-  }
+    switch (wo) {
+        case 1:
+            location.assign("/tutorials");
+            break;
+        case 2:
+            location.assign("/codepebble");
+            break;
+        case 3:
+            location.assign("/games");
+            break;
+    }
 }
-
-let socket = io.connect('http://localhost');
-socket.on('news', function (data) {
-  console.log(data);
-});
