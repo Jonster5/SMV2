e039ea38e3e870db04a946c5d73854b180a3762f
Jonster5 <49014692+Jonster5@users.noreply.github.com>
2019-10-03 21:16:04 -0400

    ping pong almost done

--- public/games/dir/pingpong/script.js
+++ public/games/dir/pingpong/script.js
@@ -13,6 +13,7 @@ class Paddle {
         this.yv = 0;
         this.height = height;
         this.width = 5;
+
     }
     move() {
         this.y += this.yv;
@@ -37,6 +38,7 @@ class Ball {
         this.yv = 0;
         this.radius = radius;
         this.color = color;
+
     }
     render() {
         ctx.fillStyle = this.color;
@@ -44,14 +46,26 @@ class Ball {
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
     }
+    init() {
+        this.xv = random(5, 10);
+        this.yv = random(5, 10);
+
+        if (random(0, 1) == 0) {
+            this.xv = -this.xv;
+        }
+
+        if (random(0, 1) == 1) {
+            this.yv = -this.yv;
+        }
+    }
     move() {
         this.x += this.xv;
         this.y += this.yv;
 
-        if (this.x - this.radius < 0) this.xv = -this.xv + Math.random() - 0.5;
-        if (this.y - this.radius < 0) this.yv = -this.yv + Math.random() - 0.5;
-        if (this.x + this.radius > canvas.width) this.xv = -this.xv + Math.random() - 0.5;
-        if (this.y + this.radius > canvas.height) this.yv = -this.yv + Math.random() - 0.5;
+        if (this.x - this.radius < 0) score(true);
+        if (this.y - this.radius < 0) this.yv = Math.abs(this.yv) + Math.random() - 0.5;
+        if (this.x + this.radius > canvas.width) score(false);
+        if (this.y + this.radius > canvas.height) this.yv = -Math.abs(this.yv) + Math.random() - 0.5;
     }
     chp(paddle, right) {
         if (right) {
@@ -66,45 +80,54 @@ class Ball {
     }
 }
 
-let player = new Paddle(480, canvas.height / 2 - 25, 70);
-let computer = new Paddle(18, canvas.height / 2 - 25, 70);
-let ball = new Ball(canvas.width / 2 - 10, canvas.height / 2 - 10, 10, "white");
+let stage;
 
-ball.xv = random(5, 10);
-ball.yv = random(5, 10);
 
-if (random(0, 1) == 0) {
-    ball.xv = -ball.xv;
+function init() {
+    stage = {
+
+        player: new Paddle(canvas.width - 20, canvas.height / 2 - 25, 70),
+        computer: new Paddle(18, canvas.height / 2 - 25, 70),
+
+        ball: new Ball(canvas.width / 2 - 10, canvas.height / 2 - 10, 10, "white")
+    };
+    stage.ball.init();
+
 }
 
-if (random(0, 1) == 1) {
-    ball.yv = -ball.yv;
-}
+
 
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    player.render("gold");
-    computer.render("red");
-    ball.render();
+    stage.player.render("gold");
+    stage.computer.render("red");
+    stage.ball.render();
 
-    computer.y = ball.y - computer.height / 2;
+    stage.computer.y = stage.ball.y - stage.computer.height / 2;
 
-    player.move();
-    computer.move();
-    ball.move();
+    stage.player.move();
+    stage.computer.move();
+    stage.ball.move();
 
-    ball.chp(player, true);
-    ball.chp(computer, false);
+    stage.ball.chp(stage.player, true);
+    stage.ball.chp(stage.computer, false);
 
-    player.move();
-    computer.move();
+    stage.player.move();
+    stage.computer.move();
 
     window.requestAnimationFrame(loop);
 }
 
 function score(plornot, no) {
+    delete stage.ball;
+    if (plornot) {
 
+    } else {
+
+    }
+    stage.ball = new Ball(canvas.width / 2 - 10, canvas.height / 2 - 10, 10, "white");
+    stage.ball.init();
 }
 
 canvas.onmousemove = function(e) {
@@ -113,7 +136,7 @@ canvas.onmousemove = function(e) {
 
 
 
-    if (y > canvas.height - player.height / 2) y = canvas.height - player.height / 2;
+    if (y > canvas.height - stage.player.height / 2) y = canvas.height - stage.player.height / 2;
     if (y <= 0) y = 0;
-    player.y = y - player.height / 2;
+    stage.player.y = y - stage.player.height / 2;
 }
